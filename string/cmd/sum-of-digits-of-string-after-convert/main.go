d126ad30cdebafa1a1eba4e8f18e77cbd06e5cee
Too Chow <tu.ngocchau@gmail.com>
2024-09-04 06:49:34 +0900

    sum-of-digits-of-string-after-convert

--- string/cmd/sum-of-digits-of-string-after-convert/main.go
+++ string/cmd/sum-of-digits-of-string-after-convert/main.go
@@ -1,10 +1,7 @@
 package main
 
 import (
-	"container/list"
 	"fmt"
-	"math"
-	"strconv"
 )
 
 // @WIP
@@ -12,47 +9,32 @@ func main() {
 	fmt.Printf("%d\n", getLucky("iiii", 1))
 	fmt.Printf("%d\n", getLucky("leetcode", 2))
 }
+
 func getLucky(s string, k int) int {
-	numStack := list.New()
-	for index, _ := range s {
-		num, err := strconv.Atoi(string(s[index]))
-		if err != nil {
-			return 0
-		}
-		if num < 10 {
-			numStack.PushBack(num)
-			continue
-		}
-		if num >= 10 {
-			numStack.PushBack(num % 10)
-			numStack.PushBack(math.Round(float64(num) / 10))
-			continue
-		}
-	}
-	count := 0
+	convertedStr := convert(s)
 	out := 0
-	for {
-		if numStack.Len() == 0 {
-			break
-		}
-		if count == k {
-			break
-		}
-		count += 1
-		out += transform(numStack)
+	for i := 0; i < k; i++ {
+		out += transform(convertedStr)
 	}
-	return transform(numStack)
+	return out
 }
 
-func transform(stack *list.List) int {
+func convert(s string) int {
+	out := 0
+	for _, c := range s {
+		out += int(c - '0')
+	}
+	return out
+}
+
+func transform(value int) int {
 	out := 0
 	for {
-		num := stack.Back().Value.(int)
-		out += num
-		stack.Remove(stack.Back())
-		if stack.Len() == 0 {
+		if value == 0 {
 			break
 		}
+		out += value % 10
+		value /= 10
 	}
 	return out
 }
