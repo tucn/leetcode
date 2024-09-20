fe545934f5bfbea02207b66bdc93b816413731e6
Ngoc-Tu Chau <chau@moadata.co.kr>
2024-09-20 09:11:06 +0900

    smallest-number with slices

--- codility/cmd/smallest-number-count/main.go
+++ codility/cmd/smallest-number-count/main.go
@@ -9,8 +9,37 @@ import (
 func main() {
 	fmt.Printf("%v\n", smallest(16))
 	fmt.Printf("%v\n", smallest(50))
+	fmt.Printf("%v\n", smallest2(16))
+	fmt.Printf("%v\n", smallest2(50))
 }
 
+// Using slices instead of stack
+func smallest2(N int) int {
+	out := make([]int, 0)
+	for N > 9 {
+		out = append(out, 9)
+		N -= 9
+	}
+	if N > 0 {
+		out = append(out, N)
+	}
+	// Reverse the out
+	for i, j := 0, len(out)-1; i < j; i, j = i+1, j-1 {
+		out[i], out[j] = out[j], out[i]
+	}
+	// Change to string
+	outStr := ""
+	for i := 0; i < len(out); i++ {
+		outStr += fmt.Sprintf("%d", out[i])
+	}
+	outInt, err := strconv.Atoi(outStr)
+	if err != nil {
+		return 0
+	}
+	return outInt
+}
+
+// Using stack
 func smallest(N int) int {
 	// Create a stack for storing number
 	stack := list.New()
